15691e51d3d6e10d842e6cd1749cfd558b583c4f
Analucia M <83628310+anilu-221@users.noreply.github.com>
2025-07-16 11:10:43 -0600

    remove duplicated files

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,6 @@ let mix = require('laravel-mix');
 
 mix.js('src/app.js', 'js')
     .sass('src/app.scss', 'css')
-	.copyDirectory('src/audio', 'dist/audio')
     .setPublicPath('dist')
     .options({
         processCssUrls: false,
